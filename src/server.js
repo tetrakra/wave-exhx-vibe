6edccffbb95bf9b641b3f205647e019cee6381ae
Nadya Riddick <nadya.riddick@gmail.com>
2024-06-07 14:49:47 -0700

    middlewares

--- src/server.js
+++ src/server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+
+const app = express();
+const PORT= 8998;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, '../public/')));
